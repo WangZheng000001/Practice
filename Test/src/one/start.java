60a03f9830f3753431003abdc54306d9285e8642
wangzheng <wangzheng000001@gmail.com>
2022-05-25 00:01:56 +0900

    test

--- Test/src/one/start.java
+++ Test/src/one/start.java
@@ -5,7 +5,9 @@ public class start {
 	public static void main(String[] args) {
 		String id = "TS111C003";
 		String password = "fdsaqwer";
-		
+
+		System.out.println("AAAAAS");
+		System.out.println("bbbbbe");
 		//TODO:在这里调用Idpassword类里的check方法
 		
 	}
